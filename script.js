9288f28be7df1bb3c1c634ab35b0efc58d7dafa2
Surya Narayanan <125539401+suryadsign@users.noreply.github.com>
2024-03-13 17:52:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// var playBtn = document.getElementById('play'),
-//   heartbeat = document.getElementById('rocket')
-// 	audios = document.querySelectorAll('audio');
-// console.log(audios);
+var playBtn = document.getElementById('play'),
+  heartbeat = document.getElementById('rocket')
+	audios = document.querySelectorAll('audio');
+console.log(audios);
 
 
-// playBtn.addEventListener('mouseover', function() {
-// [].forEach.call(audios, function(audio) {
-//   // do whatever
-//   audio.play();
-// });
-// }, false);
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  // do whatever
+  audio.play();
+});
+}, false);
 
-// playBtn.addEventListener('mouseleave', function() {
-//   heartbeat.pause();
-//   heartbeat.currentTime = 0;
-// }, false);
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
