05c4d641d67671068c9c192dd8536f936e6240b5
Surya Narayanan <125539401+suryadsign@users.noreply.github.com>
2024-03-18 18:22:14 +0530

    Add files via upload
    Smooth scrolling and text animation done.

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-var playBtn = document.getElementById('play'),
-  heartbeat = document.getElementById('rocket')
-	audios = document.querySelectorAll('audio');
-console.log(audios);
+  gsap.registerPlugin(ScrollTrigger)
+  const splitTypes = document.querySelectorAll('.heading,.quote')
+
+  splitTypes.forEach((char,i) => {
+    const text = new SplitType (char, { types: 'chars,words'})
+
+    gsap.from(text.chars, {
+      scrollTrigger : {
+        trigger:char,
+        start:'top 70%',
+        end:'top 20%',
+        scrub:false,
+        markers:false
+      },
+      y:100,
+      opacity:0,
+      stagger:0.08,
+      duration:0.1
 
 
-playBtn.addEventListener('mouseover', function() {
-[].forEach.call(audios, function(audio) {
-  // do whatever
-  audio.play();
-});
-}, false);
+    })
+  })
 
-playBtn.addEventListener('mouseleave', function() {
-  heartbeat.pause();
-  heartbeat.currentTime = 0;
-}, false);
+
+  const lenis = new Lenis()
+
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
+
+  lenis.on('scroll', ScrollTrigger.update)
+
+  gsap.ticker.add((time)=>{
+    lenis.raf(time * 1000)
+  })
+
+  gsap.ticker.lagSmoothing(0)
+
