3e754e3f31d638e19738f4580c24347be627ac09
Surya Narayanan <125539401+suryadsign@users.noreply.github.com>
2024-04-13 23:48:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,116 +1,133 @@
 gsap.registerPlugin(ScrollTrigger)
-const splitTypes = document.querySelectorAll('.quote,.nameDescription,.heading,')
+const splitTypes = document.querySelectorAll('.quote,.nameDescription,.heading')
 
-splitTypes.forEach((char, i) => {
-  const text = new SplitType(char, { types: 'chars,words' })
+splitTypes.forEach((char,i) => {
+  const text = new SplitType (char, { types: 'chars,words'})
 
   gsap.from(text.chars, {
-    scrollTrigger: {
-      trigger: char,
-      start: 'top 50%',
-      end: 'top 20%',
-      scrub: false,
-      markers: false,
-
+    scrollTrigger : {
+      trigger:char,
+      start:'top 80%',
+      end:'top 20%',
+      scrub:false,
+      markers:false,
+      
     },
-    y: -100,
-    opacity: 0,
-    duration: 0.5,
+    y:-100,
+    opacity:0,
+    duration:0.5,
+    stagger:0.02
 
   })
 })
 
-let tl = gsap.timeline({
+let tl= gsap.timeline({
+  scrollTrigger:{
+    trigger:'.approaches',
+    start:'35% 80%',
+    end:'bottom 20%',
+    scrub:true,
+    markers:false
+  }
+  })
+
+tl.from('.approaches',{
+  opacity:0,
+  x:200,
+  duration:1
+})
+
+gsap.from('.expertises',{
+  x:200,
+  opacity:0.2,
+  duration:1.5,
   scrollTrigger: {
-    trigger: '.approaches',
-    start: '35% 80%',
-    end: 'bottom 20%',
-    scrub: true,
-    markers: false
+    trigger:'.expertises',
+    start:'top 80%',
+    end:'center 20%',
+    scrub:true,
+    markers:false
   }
 })
 
-tl.from('.approaches', {
-  opacity: 0,
-  x: 200,
-  duration: 1
-})
+gsap.to('.a',{
+  x:100,
+  y:0,
+  duration:10,
 
-gsap.from('.expertises', {
-  x: 200,
-  opacity: 0.2,
-  duration: 1.5,
+  repeat:-1,
   scrollTrigger: {
-    trigger: '.expertises',
-    start: 'top 80%',
-    end: 'center 20%',
-    scrub: true,
-    markers: false
+    trigger:'.glassgroup',
+    start:'top center',
+    end:'bottom center',
+    scrub:false,
+    markers:false
+
   }
 })
 
-gsap.from('.b', {
-  x: 400,
-  y: 0,
-  duration: 10,
-  repeat: -1,
+gsap.from('.b',{
+  x:400,
+  y:0,
+  duration:10,
+  repeat:-1,
   scrollTrigger: {
-    trigger: '.glassgroup',
-    start: 'top center',
-    end: 'bottom center',
-    scrub: false,
-    markers: false
+    trigger:'.glassgroup',
+    start:'top center',
+    end:'bottom center',
+    scrub:false,
+    markers:false
   }
 })
 
-gsap.from('.c', {
-  x: -200,
-  y: 0,
-  duration: 10,
-  repeat: -1,
+gsap.from('.c',{
+  x:-200,
+  y:0,
+  duration:10,
+  repeat:-1,
   scrollTrigger: {
-    trigger: '.glassgroup',
-    start: 'top center',
-    end: 'bottom center',
-    scrub: false,
-    markers: false,
+    trigger:'.glassgroup',
+    start:'top center',
+    end:'bottom center',
+    scrub:false,
+    markers:false,
   }
 })
 
-gsap.from('.aa,.bb', {
-  y: 100,
-  opacity: 0.2,
-  duration: 2,
+gsap.from('.aa,.bb',{
+  y:100,
+  opacity:0.2,
+  duration:2,
   scrollTrigger: {
-    trigger: '.projects',
-    start: 'top 80%',
-    end: 'bottom center',
-    scrub: true,
-    markers: false
+    trigger:'.projects',
+    start:'top 80%',
+    end:'bottom center',
+    scrub:true,
+    markers:false
   }
 })
 
-gsap.from('.cc,.dd', {
-  y: 50,
-  opacity: 0.1,
-  duration: 1,
+gsap.from('.cc,.dd',{
+  y:50,
+  opacity:0.1,
+  duration:1,
   scrollTrigger: {
-    trigger: '.cc',
-    start: 'top 80%',
-    end: 'bottom 20%',
-    scrub: true,
-    markers: false
+    trigger:'.cc',
+    start:'top 80%',
+    end:'bottom 20%',
+    scrub:true,
+    markers:false
   }
 })
 
 
-gsap.from('.icon', {
-  y: 50,
-  opacity: 0,
+gsap.from('.icon',{
+  y:50,
+  opacity:0,
   scrollTrigger: {
-    trigger: '.icon',
-    scrub: false,
-    markers: false
+    trigger:'.icon',
+    scrub:false,
+    markers:false
   }
 })
 
@@ -123,30 +140,31 @@ lenis.on('scroll', (e) => {
 
 lenis.on('scroll', ScrollTrigger.update)
 
-gsap.ticker.add((time) => {
+gsap.ticker.add((time)=>{
   lenis.raf(time * 1000)
 })
 
 gsap.ticker.lagSmoothing(0)
 
 
-function smoothScroll() {
+function smoothScroll(){
   document.querySelector('#works').scrollIntoView({
-    behavior: 'smooth'
+      behavior: 'smooth'
   });
 }
 
-function InteractionScroll() {
-  document.querySelector('#interactives').scrollIntoView({
+function InteractionScroll(){
+document.querySelector('#interactives').scrollIntoView({
     behavior: 'smooth'
-  });
+});
 }
 
 let play = document.querySelector('.play');
 let audioArr = document.getElementsByTagName('audio');
-console.log(play)
-play.addEventListener('mouseenter', () => {
-  audioArr[0].play()
+console.log(audioArr);
+
+play.addEventListener('mouseenter', ()=> {
+audioArr[0].play()
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
